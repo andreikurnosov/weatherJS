d7f7766f1f35932d7e701975e40a488d887ca385
nixdie <nixdie@yandex.ru>
2018-03-22 04:44:20 +0700

    create html markup

---
+++ weather.js
@@ -0,0 +1,25 @@
+class Weather{
+  constructor(){
+    this.apiKey = 'f5f69a3bb603603b';
+    this.city = city;
+    this.state = state;
+  }
+
+  // Fetch weather from API
+  async getWeather(){
+    const response = await fetch(`http://api.wunderground.com/api/${this.apiKey}/conditions/q/${this.state}/${this.city}.json`);
+
+    const responseData = await response.json();
+
+    return responseData.current_observation;
+  }
+
+  // Change weather location
+  changeLocation(city, state){
+    this.city = city;
+    this.state = state;
+  }
+}
+
+
+
