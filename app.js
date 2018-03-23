3f06c0e052ed1e32b32ba0b09bce068a2287de5f
nixdie <nixdie@yandex.ru>
2018-03-23 14:42:03 +0700

    display the weather

--- app.js
+++ app.js
@@ -1,13 +1,17 @@
 // Init weather object
 const weather = new Weather('Boston', 'MA');
 
+// Init UI
+const ui = new UI();
+
 // weather.changeLocation('Miami', 'FL');
 
 document.addEventListener('DOMContentLoaded', getWeather);
 
 function getWeather(){
   weather.getWeather()
-  .then(results => {console.log(results);
+  .then(results => {
+    ui.paint(results);
   })
   .catch(err => console.log(err));
 }
